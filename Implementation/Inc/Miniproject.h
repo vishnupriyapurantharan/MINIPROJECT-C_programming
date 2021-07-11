b78e5e00fdf3ccd6b4b188e97a376d2febb71589
VISHNU PRIYA P <36762668+vishnupriyapurantharan@users.noreply.github.com>
2021-07-11 22:39:24 +0530

    Update Miniproject.h

--- Implementation/Inc/Miniproject.h
+++ Implementation/Inc/Miniproject.h
@@ -1,1 +1,28 @@
+#ifndef __MINIPROJ_H__ 
+#define __MINIPROJ_H__
 
+long int Bin_to_Dec(long int); //1:BINARY TO DECIMAL
+
+long int Bin_to_Oct(long int); //2:BINARY TO OCTAL
+
+long int Bin_to_Hex(long int); //3:BINARY TO HEXA-DECIMAL
+
+long int Dec_to_Bin(long int); //4:DECIMAL TO BINARY
+
+long int Dec_to_Oct(long int); //5:DECIMAL TO OCTAL
+
+long int Dec_to_Hex(long int); //6:DECIMAL TO HEXA-DECIMAL
+
+long int Oct_to_Bin(long int); //7:OCTAL TO BINARY
+
+long int Oct_to_Dec(long int); //8:OCTAL TO DECIMAL
+
+long int Oct_to_Hex(long int); //9:OCTAL TO HEXA-DECIMAL
+
+void Hex_to_Bin(char []); //10:HEXA-DECIMAL TO BINARY
+
+void Hex_to_Dec(char []); //11:HEXA-DECIMAL TO DECIMAL
+
+void Hex_to_Oct(char []); //12:HEXA-DECIMAL TO OCTAL
+
+#endif 
